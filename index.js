f57a093138546083738f0408230fed2122b2c6c2
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-19 15:58:16 +0300

    4.3

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const http = require('http') 
-const express = require('express') 
-const app = express() 
-const bodyParser = require('body-parser') 
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware') 
+const middleware = require('./utils/middleware')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -17,8 +17,8 @@ mongoose
     console.log('connected to database', process.env.MONGODB_URI)
   })
 
-app.use(cors()) 
-app.use(bodyParser.json()) 
+app.use(cors())
+app.use(bodyParser.json())
 app.use(middleware.logger)
 
 app.use('/api/blogs', blogsRouter)
