8d71a1b64fb78534cd2af3652b30ae317075fffc
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-19 19:30:10 +0300

    4.9 ja 4.8 fix

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ if (process.env_NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-let port = process.env.port
+let port = process.env.PORT
 let mongoUrl = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
