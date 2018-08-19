66094e640bab102b75a43962cd9d2e6346c6a84a
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-19 16:10:37 +0300

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const total = (items, value) => {
+    return items.reduce((a, b) => {
+      return a + b[value]
+    }, 0)
+  }
+  return total(blogs, 'likes')
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
